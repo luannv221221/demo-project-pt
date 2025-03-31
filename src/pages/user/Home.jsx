f3226aab6658e7d795fb693238a4ac595e4a1472
your new username <smallmonster900@gmail.com>
2025-03-31 20:56:19 +0700

    'a'

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container, Row } from 'react-bootstrap'
+import { getAllProduct } from '../../services/productService';
+import { Image } from 'antd';
 
 const Home = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        getAllProduct().then((response) => {
+            console.log(response);
+            setProducts(response.data)
+        }).catch(err => console.log(err));
+    }, []);
+    function getImgUrl(name) {
+        // console.log("urrl", name);
+
+        // console.log("chuaanr", new URL(`./${name}`, import.meta.url).origin);
+        // return new URL(`./${name}`, import.meta.url).href
+        return "../" + name;
+    }
+
+
     return (
-        <div>Home</div>
+        <>
+            <h1 className='text-center mt-5'>Danh sach san pham</h1>
+
+            <Container>
+                <Row>
+                    {products.map((item) => (
+                        <div className="col-lg-3" key={item.id}>
+                            <div className="card">
+                                {/* <img className="card-img-top" src="{}" alt="Title" /> */}
+                                {/* <Image src={item.image}></Image> */}
+                                <img src={item.image} />
+                                <div className="card-body">
+                                    <h4 className="card-title">{item.productName}</h4>
+                                    <p className="card-text">{item.price}</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    ))}
+
+                </Row>
+            </Container>
+
+        </>
     )
 }
 
--- src/services/productService.js
+++ src/services/productService.js
@@ -1,7 +1,12 @@
-import { BASE_URL_ADMIN } from "../api"
+import { BASE_URL, BASE_URL_ADMIN } from "../api"
 
 export const postProductAPI = async (data) => {
     const response = await BASE_URL_ADMIN["post"]('/product', data);
     console.log(response);
+    return response;
+}
+
+export const getAllProduct = async () => {
+    const response = await BASE_URL["get"]("/products");
     return response;
 }
